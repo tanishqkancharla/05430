d800e65b1633e8063f61164d52b6f12457ea818a
Tanishq <tklightningfast@gmail.com>
2020-03-06 23:12:04 -0500

    added javascript for product page

---
+++ Homework/HW5/productpage.js
@@ -0,0 +1,22 @@
+function updatePrice() {
+	var amount_selector_list = document.getElementById("amount-selector-list");
+	var glaze_selector_list = document.getElementById("glaze-selector-list");
+	var unitprice = 0;
+	for (let i = 0; i < glaze_selector_list.children.length; i++) {
+		var radio = glaze_selector_list.children[i].children[0];
+		if (radio.checked == true) {
+			unitprice = Number(radio.value);
+		}
+	}
+	if (unitprice == 0) return;
+	var price = 0;
+	for (let i = 0; i < amount_selector_list.children.length; i++) {
+		var radio = amount_selector_list.children[i].children[0];
+		if (radio.checked == true) {
+			price = Number(radio.value) * unitprice;
+		}
+	}
+	if (price == 0) return;
+	console.log("got here");
+	document.getElementById("price-display").innerHTML = "Price: $" + price;
+}
